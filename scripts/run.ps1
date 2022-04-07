8b0cad98be917b4650e8cac6d840460fac0e04c5
Jan Löliger <jan.loeliger@gmail.com>
2022-04-07 09:42:04 +0200

    added scripts for compiling and execution

---
+++ scripts/run.ps1
@@ -0,0 +1,1 @@
+java -jar ../target/library-1.0-SNAPSHOT.jar
--- src/main/java/ch/project/library/Main.java
+++ src/main/java/ch/project/library/Main.java
@@ -3,6 +3,6 @@ package ch.project.library;
 public class Main {
     public static void main(String args[])  
     {  
-    
+        System.out.println("sali");
     }  
 }
