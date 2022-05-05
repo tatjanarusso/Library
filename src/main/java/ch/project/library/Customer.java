f50ba61d182b66983d4cf48516c8dbcb36d0f90e
Tatjana <tatjana.russo@edu.tbz.ch>
2022-05-05 10:44:13 +0200

    Customer class added

---
+++ src/main/java/ch/project/library/Customer.java
@@ -0,0 +1,20 @@
+package ch.project.library;
+
+import java.util.Scanner;
+
+public class Customer {
+
+    public void searchBook(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Buch suchen:");
+        String bookTitle = sc.next();
+
+       /* if (bookTitle exits in db){
+       list book
+       }
+       else {
+       list similar books => category
+       }
+        */
+    }
+}
